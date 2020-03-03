d3518958260f471860ef7623c786ecc063936f39
gulatisagar2 <51478838+gulatisagar2@users.noreply.github.com>
2020-03-03 20:28:46 +0530

    last

---
+++ cmddepoymentscript.sql
@@ -0,0 +1,1 @@
+﻿SqlPackage.exe /Action:Publish /SourceFile:"C:\Users\saggulat\source\repos\DacpacSQL\Snapshots\DacpacSQL_20200303_19-13-57.dacpac" /TargetDatabaseName:DacpacSQL /TargetServerName:"WKWIN6750825"
